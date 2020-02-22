0265a859ee5dd176196bc62dbd1939a2a9a60a3a
Your Name In Single Quotes <your email here in single quotes>
2020-02-22 23:31:32 +0200

    update on About-me by adding CSS stylish

--- js/app.js
+++ js/app.js
@@ -88,18 +88,25 @@ switch (study.toLowerCase()) {
         alert('I\'m really sorry  !');
         break;
 }
-
-var game = prompt('This is guess game : choose number', '0 through 9');
-var LOOP = 1;
 var rand = (Math.floor(Math.random() * 10));
+console.log(rand);
+var game = parseInt(prompt('This is guess game : choose number', '0 through 9'));
+
+
+var LOOP = 1;
+
+
 if (game == rand) {
+
     alert('YOU ARE AWESOME THAT RIGHT');
+
+
     score = score + 1;
 
 } else {
     for (LOOP; LOOP <= 4; LOOP++) {
-        game = prompt(`Attempt ${LOOP} This is guess game : choose number`, '0 through 9');
-
+        game = parseInt(prompt('This is guess game : choose number', '0 through 9'));
+           
         if (game < rand) {
             alert('TRY AGAIN THAT wrong');
             alert('Too low');
@@ -117,51 +124,34 @@ if (game == rand) {
     alert(`The right number is ${rand}`);
 }
 
-var Length_array = 0;
+
 var array = ['mohamad', 'ahmad', 'ammar', 'amjad'];
 
 
 var game2 = prompt('Guess a name :', 'It\'s name').toLowerCase();
-var LOOP2 = 1;
 
-    for(LOOP2 ; LOOP2 < 7 ; LOOP2++){
-    if(game2 === array[Length_array]) {
-       
-            console.log('YOU ARE REALLYYYYYYYY AWESOME THAT RIGHT');
-            alert('YOU ARE REALLYYYYYYYY AWESOME THAT RIGHT');
-            score = score + 1;
-            break;
-        } else if (game2 !== array[Length_array]){
-            alert('TRY AGAIN THAT wrong');
-            game2= prompt('Guess a name :', 'It\'s name').toLowerCase();
-           if (game2 === array[Length_array]){
-            console.log('YOU ARE REALLYYYYYYYY AWESOME THAT RIGHT');
-            alert('YOU ARE REALLYYYYYYYY AWESOME THAT RIGHT');
-            score = score + 1;
-            break;
-           } else if(game2 !== array[Length_array]){
-            alert('TRY AGAIN THAT wrong');
-            break;
-           }
-                 
 
+
+
+for (var i = 0; i < 5; i++) {
+
+    for (var j = 0; j < array.length; j++) {
+
+        if (game2 == array[j]) {
+            console.log(game2);
+            alert('true');
+            i = 5;
             break;
+
         }
-              
     }
 
+    if (i == 5) {
+        break;
+    }
 
+    game2 = prompt('try again');
 
-    alert(`The right name is ${array[Length_array]}`);
-
-
+}
+alert(`The right name is ${array}`);
 alert(`Your score is ${score} out of 7`);
-// save question and other things in array
-// var save =[];
-// var row =0;
-// var cloumn =0;
-// for (row ; row<5 ;row++){
-//     for(cloumn ; cloumn < 5 ; cloumn ++){
-//         save=[row];
-//     }
-// }
